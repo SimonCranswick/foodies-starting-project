9b7ff38ebd7b23583b498ca9d07926fbeaa0a90a
SimonCranswick <115464797+SimonCranswick@users.noreply.github.com>
2024-03-25 15:35:16 +0000

    Postgres implementation 2

--- initdb.js
+++ initdb.js
@@ -1,5 +1,5 @@
-const sql = require('better-sqlite3');
-const db = sql('meals.db');
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
 
 const dummyMeals = [
   {
@@ -164,18 +164,25 @@ const dummyMeals = [
   },
 ];
 
-db.prepare(`
-   CREATE TABLE IF NOT EXISTS meals (
-       id INTEGER PRIMARY KEY AUTOINCREMENT,
-       slug TEXT NOT NULL UNIQUE,
-       title TEXT NOT NULL,
-       image TEXT NOT NULL,
-       summary TEXT NOT NULL,
-       instructions TEXT NOT NULL,
-       creator TEXT NOT NULL,
-       creator_email TEXT NOT NULL
-    )
-`).run();
+async function GET(request: Request) {
+   try {
+      const result = await sql `
+      CREATE TABLE IF NOT EXISTS meals (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          slug TEXT NOT NULL UNIQUE,
+          title TEXT NOT NULL,
+          image TEXT NOT NULL,
+          summary TEXT NOT NULL,
+          instructions TEXT NOT NULL,
+          creator TEXT NOT NULL,
+          creator_email TEXT NOT NULL
+       );`;
+       return NextResponse.json({ result }, { status: 200 });
+      }
+      catch (error) {
+         return NextResponse.json({ error }, { status: 500 });
+       }
+   }
 
 async function initData() {
   const stmt = db.prepare(`
